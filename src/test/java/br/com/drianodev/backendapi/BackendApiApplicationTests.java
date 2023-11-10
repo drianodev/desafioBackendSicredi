a82f6d8b1a48bc92ea97f5c05987ffc940e39d3e
drianodev <adrianomendes661@gmail.com>
2023-11-10 15:36:59 -0300

    first commit

---
+++ src/test/java/br/com/drianodev/backendapi/BackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.drianodev.backendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
